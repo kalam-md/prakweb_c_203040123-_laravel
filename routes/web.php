0d51d6a66ae1ee04759dbf66c984e91f6e4511f3
kaleumlam <akunkerjakalam@gmail.com>
2022-10-17 19:01:54 +0700

    laravel : video ke-3 -> struktur folder, routes & views

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Kalam",
+        "email" => "kalam@mail.com",
+        "image" => "kalam.png",
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
