871019fcf1210d2257b1ece0dcf8bd674328e9a5
kaleumlam <akunkerjakalam@gmail.com>
2022-10-24 10:44:37 +0700

    laravel : video ke-14 -> view login & register

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 use App\Models\Category;
-use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -43,3 +44,6 @@ Route::get('/categories', function () {
         'categories' => Category::all(),
     ]);
 });
+
+Route::get('/login', [LoginController::class, 'index']);
+Route::get('/register', [RegisterController::class, 'index']);
